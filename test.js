423379a22e9433eae83e856ffa03d2844ca00304
Bryan C Green <bcgreen@gmail.com>
2021-06-16 20:10:37 -0500

    Improved test file

--- test.js
+++ test.js
@@ -18,9 +18,22 @@ let test = {
   }
 }; 
 
-console.log(searchobj(test, 'info'));
-console.log(searchobj(test, 'address'));
-console.log(searchobj(test, 'home'));
-console.log(searchobj(test, 'phone_numbers'));
+console.log("Testing with this object: ");
+console.log(test);
+console.log("====================================");
 
+console.log("Search for 'info' returns: ");
+console.log(searchobj(test, 'info'));
+console.log("====================================");
+
+console.log("Search for 'address' returns: ");
+console.log(searchobj(test, 'address'));
+console.log("====================================");
+
+console.log("Search for 'phone_numbers' returns: ");
+console.log(searchobj(test, 'phone_numbers'));
+console.log("====================================");
+
+console.log("Search for 'home' returns: ");
+console.log( searchobj(test, 'home'));
 
